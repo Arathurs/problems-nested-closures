7bbc07f851a95d72eca665eee68e3d22e414920a
Royalthius <arathurs@gmail.com>
2020-12-16 22:23:57 -0500

    Commit and push progress

--- closureProblems.js
+++ closureProblems.js
@@ -1,4 +1,7 @@
 
+//Two Problems with Closures
+//Suppose we have a function that processes a large amount of data, and we choose to go the callback hell route. There are two main issues:
+//1). The big data variable is alive until the end of the nested closure, or rather the innermost closure. This prevents the garbage collector on client can't clean the data up. 
 function process (bigdata, cb) {
     remoteCall(bigdata, function(err, something) {
         storeSomething(something, function (err, res) {
